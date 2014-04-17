ca37a52b531ba05781a05d26a4512a6835a46277
Jose Luis Rivas <me@ghostbar.co>
2014-04-17 02:43:38 -0500

    test-timelines: updated

--- test/test-timelines.js
+++ test/test-timelines.js
@@ -1,49 +1,36 @@
-var Twitter = require('../index'),
-    config = require('./config.json'),
-    should = require('should');
+/* global describe */
+/* global it */
+'use strict';
 
-describe('Twitter.Timelines Functions:', function() {
-  describe('Timelines loading', function() {
-    it('should create the Object', function() {
+var Twitter = require('../index');
+var optional = require('optional');
+var optConfig = {consumer_key: 'random', consumer_secret: 'random', token: 'random', token_secret: 'random'};
+var config = optional('./config.json') || optConfig;
+var should = require('should');
+
+describe('Twitter.Timelines Functions:', function () {
+
+  describe('Timelines loading', function () {
+    it('should create the Object', function () {
       var timelines = new Twitter.Timelines(config);
+
+      should.exist(timelines.Timelines);
 
       timelines.should.be.an.instanceOf(Object);
     });
   });
 
-  describe('Timelines.mentions()', function() {
-    it('should be implemented, not yet', function() {
-      console.log('TODO: TO BE IMPLEMENTED!');
-    });
-  });
+  describe('Timelines.user()', function () {
+    it('should return an error for missing screen_name', function (done) {
+      var timelines = new Twitter.Timelines(config);
+      var params = { count: 1 };
 
-  describe('Timelines.user()', function() {
-    it('should request an user timeline', function(done) {
-      var timelines = new Twitter.Timelines(config),
-          params = {
-            screen_name: 'ghostbar',
-            count: 1
-          };
-
-      timelines.user(params, function(err, response) {
-        should.not.exist(err);
-
-        should.exist(response);
+      timelines.user(params, function (err) {
+        should.exist(err);
 
         done();
       });
     });
   });
 
-  describe('Timelines.home()', function() {
-    it('should be implemented, not yet', function() {
-      console.log('TODO: TO BE IMPLEMENTED!');
-    });
-  });
-
-  describe('Timelines.retweets()', function() {
-    it('should be implemented, not yet', function() {
-      console.log('TODO: TO BE IMPLEMENTED!');
-    });
-  });
 });
