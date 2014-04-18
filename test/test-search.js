62c98cb132992273026c95cb97a424f96ebb249b
Jose Luis Rivas <me@ghostbar.co>
2014-04-18 01:48:25 -0500

    test-search: rewrote

--- test/test-search.js
+++ test/test-search.js
@@ -1,39 +1,32 @@
-var Twitter = require('../index'),
-    config = require('./config.json'),
-    should = require('should');
+/* global describe */
+/* global it */
+'use strict';
 
-describe('Twitter.Search Functions:', function() {
-  describe('Search loading', function() {
-    it('should create the Object', function() {
+var Twitter = require('../index');
+var optional = require('optional');
+var optConfig = {consumer_key: 'random', consumer_secret: 'random', token: 'random', token_secret: 'random'};
+var config = optional('./config.json') || optConfig;
+var should = require('should');
+
+describe('Twitter.Search Functions:', function () {
+  describe('Search loading', function () {
+    it('should create the Object', function () {
       var search = new Twitter.Search(config);
 
       search.should.be.an.instanceOf(Object);
     });
   });
 
-  describe('Search.query()', function() {
+  describe('Search.query()', function () {
     var search = new Twitter.Search(config);
 
-    it('should return an error about missing q in params', function(done) {
-      search.query({}, function(err, response) {
+    it('should return an error about missing q in params', function (done) {
+      search.query({}, function (err) {
         should.exist(err);
 
         done();
       });
     });
 
-    it('should return an Object with statuses', function(done) {
-      this.timeout(40000);
-
-      search.query({q: 'hola', count: 2}, function(err, response) {
-        should.not.exist(err);
-
-        should.exist(response.statuses);
-
-        response.statuses.should.be.an.instanceOf(Array);
-
-        done();
-      });
-    });
   });
 });
