c3d9199ea73e40175ee22449b6408cfd1d4399c5
Jose Luis Rivas <me@ghostbar.co>
2014-04-30 02:23:33 -0500

    test-tweets: cleanup for only no-net methods

--- test/test-tweets.js
+++ test/test-tweets.js
@@ -1,6 +1,10 @@
-var Twitter = require('../index'),
-    config = require('./config.json'),
-    should = require('should');
+/* global it */
+/* global describe */
+'use strict';
+
+var Twitter = require('../index');
+var config = require('./config.json');
+var should = require('should');
 
 describe('Twitter.Tweets Functions:', function() {
   describe('Tweets loading', function() {
@@ -12,10 +16,10 @@ describe('Twitter.Tweets Functions:', function() {
   });
 
   describe('Tweets.retweets()', function() {
-    var tweets = new Twitter.Tweets(config),
-        params = {
-          count: 1
-        };
+    var tweets = new Twitter.Tweets(config);
+    var params = {
+      count: 1
+    };
 
     it('should return an error about missing id', function(done) {
       tweets.retweets(null, params, function(err, response) {
@@ -25,16 +29,6 @@ describe('Twitter.Tweets Functions:', function() {
       });
     });
 
-    it('should return one of the collection of retweets on an status', function(done) {
-
-      tweets.retweets('394732926832623616', params, function(err, response) {
-        should.not.exist(err);
-
-        should.exist(response);
-
-        done();
-      });
-    });
   });
 
   describe('Tweets.show()', function() {
@@ -48,15 +42,6 @@ describe('Twitter.Tweets Functions:', function() {
       });
     });
 
-    it('should return a single tweet', function(done) {
-      tweets.show('394732926832623616', null, function(err, response) {
-        should.not.exist(err);
-
-        should.exist(response);
-
-        done();
-      });
-    });
   });
 
   describe('Tweets.destroy()', function() {
@@ -70,9 +55,6 @@ describe('Twitter.Tweets Functions:', function() {
       });
     });
 
-    it('Success call should be implemented, not yet', function() {
-      console.log('TODO: TO BE IMPLEMENTED!');
-    });
   });
 
   describe('Tweets.update()', function() {
@@ -88,36 +70,21 @@ describe('Twitter.Tweets Functions:', function() {
 
     it('should return an error about too much characters', function(done) {
       tweets.update({
-        status: "Stupidly long status so this fails, I hope this is enough because I'm actually not counting how much characters I'm writing. I'm gonna make it extra-longh just to be sure althought it should be just 141 so ... Anyway, this should work."
-      }, function(err, response) {
+        status: 'Stupidly long status so this fails, I hope this is enough because I\'m actually not counting how much characters I\'m writing. I\'m gonna make it extra-longh just to be sure althought it should be just 141 so ... Anyway, this should work.'
+      }, function(err) {
         should.exist(err);
 
         done();
       });
     });
 
-    it('Should send a tweet', function(done) {
-      this.timeout(40000);
-      tweets.update({
-        status: "123... This is a test"
-      }, function(err, response) {
-        should.not.exist(err);
-
-        should.exist(response);
-
-        should.exist(response.id);
-
-        done();
-      });
-
-    });
   });
 
   describe('Tweets.retweet()', function() {
     var tweets = new Twitter.Tweets(config);
 
     it('should return an error about missing id', function(done) {
-      tweets.retweet(null, null, function(err, response) {
+      tweets.retweet(null, null, function(err) {
         should.exist(err);
 
         done();
@@ -139,49 +106,26 @@ describe('Twitter.Tweets Functions:', function() {
     var tweets = new Twitter.Tweets(config);
 
     it('should return an error related to missing url or id', function(done) {
-      tweets.oembed({}, function(err, response) {
+      tweets.oembed({}, function(err) {
         should.exist(err);
 
         done();
       });
     });
 
-    it('should return an html', function(done) {
-      tweets.oembed({
-        id: '394732926832623616'
-      }, function(err, response) {
-        should.not.exist(err);
-        
-        should.exist(response.html);
-
-        done();
-      });
-    });
   });
 
   describe('Tweets.retweeters()', function() {
     var tweets = new Twitter.Tweets(config);
 
     it('should return an error about missing id', function(done) {
-      tweets.retweeters({}, function(err, response) {
+      tweets.retweeters({}, function(err) {
         should.exist(err);
 
         done();
       });
     });
 
-    it('should return a list of ids', function(done) {
-      tweets.retweeters({
-        id: '394732926832623616',
-        count: 1
-      }, function(err, response) {
-        should.not.exist(err);
-
-        should.exist(response.ids);
-
-        done();
-      });
-    });
   });
 
 });
